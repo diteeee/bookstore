654ec0b7f3af07837f30d1fd99bd18ad1c820bab
hava04 <149471713+diteeee@users.noreply.github.com>
2025-06-21 14:10:17 +0200

    perfundimi se si pasna bo commits te tests

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   presets: [
-    ["@babel/preset-react", { runtime: "automatic" }],
-    "@babel/preset-env",
-    "@babel/preset-typescript"
+    ["@babel/preset-react", { runtime: "automatic" }], // For React
+    "@babel/preset-env", // For modern JavaScript
+    "@babel/preset-typescript" // For TypeScript support
+  ],
+  plugins: [
+    "@babel/plugin-syntax-import-attributes" // Enables experimental import attributes
   ],
 };
